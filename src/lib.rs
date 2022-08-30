a0b5a47f2fac3165e1267ad6dd08afcb0e217b09
Sedana Yoga <9g.yoga27@gmail.com>
2022-08-30 15:45:01 +0800

    load image with image::load_from_memory

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use base64::decode;
+use image::load_from_memory;
 use wasm_bindgen::prelude::wasm_bindgen;
 use web_sys::console::log_1 as log;
 
@@ -7,5 +8,8 @@ pub fn grayscale(encoded_file: &str) {
     log(&"Grayscale is called".into());
 
     let base64_to_vector = decode(encoded_file).unwrap();
-    log(&"Image decoded".into())
+    log(&"Image decoded".into());
+
+    let img = load_from_memory(&base64_to_vector);
+    log(&"Image loaded".into())
 }
